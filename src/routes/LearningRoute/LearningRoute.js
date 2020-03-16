6f1fa42a44cc96b3657a45ebce74f39bbae86bee
Scott Lingner <slingner@gmail.com>
2020-03-15 17:09:57 -0700

    styling dashboard page

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -1,10 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
 
 class LearningRoute extends Component {
   render() {
     return (
       <section>
-        implement and style me
+        <h2 className='languageHeader'>Language: French</h2>
+        <h3 className='languageHeader'>Words to Practice</h3>
+        <div>
+          <p>Each French word here with correct/incorrect count</p>
+        </div>
+        <Link to='/'>Home</Link>
       </section>
     );
   }
