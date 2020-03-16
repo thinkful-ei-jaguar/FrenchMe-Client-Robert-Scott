05aa6839e8e2a01d42f77596967ceb984192ac34
Alex <robert.perez6@g.austincc.edu>
2020-03-15 19:37:46 -0500

    added language service to the front end

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,16 +1,53 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import LanguageService from '../../services/language-service';
+
 
 class DashboardRoute extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state={
+      language:{},
+      words:[],
+
+    }
+  }
+
+
+  componentDidMount(){
+    LanguageService.GetLanguage()
+    .then(res=>{
+      this.setState({
+        language:res.language,
+        words:res.words,
+      })
+      console.log(this.state)
+    })
+  }
+
+  mapthewords=(words)=>{
+    return words.map(w=>{
+      return <div>
+        <p>{w.original}</p>
+    <p> correct:{w.correct_count} incorrect:{w.incorrect_count}</p>
+        
+        </div>
+    })
+  }
+
+
+
+
   render() {
     return (
       <section>
-        <h2 className='languageHeader'>Language: French</h2>
+        <h2 className='languageHeader'>Language: {this.state.language.name}</h2>
         
         <h3 className='languageHeader'>Words to Practice</h3>
         <div>
-          <p>Each French word here with correct/incorrect count</p>
-        </div>
+          {this.mapthewords(this.state.words)}
+          </div>
         <Link to='/learn'>Start Practicing</Link>
       </section>
     );
