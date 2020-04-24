83ab572fac305b99e894bf0ece542105ef563933
Scott Lingner <slingner@gmail.com>
2020-04-24 02:17:24 -0700

    add text to landing

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -7,9 +7,7 @@ export default function Home() {
   return (
     <div className="homeWrap">
       <div className="page-wrapper">
-        <p style={{ fontSize: '45px', marginTop: '15px' }}>
-          Let's practice French!
-        </p>
+        <p className="practiceFrench">Let's practice French!</p>
         <Animated animationIn="slideInLeft" animationInDuration={1750}>
           <Biker className="biker" />
         </Animated>
